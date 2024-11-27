06c145b55d26e087cc3d2785d70b3bfaf34ad324
nhankhtn <nguyenanhnguyen3006@gmail.com>
2024-11-27 23:33:52 +0700

    update syscall trace

--- user/attack.c
+++ user/attack.c
@@ -6,8 +6,6 @@
 int
 main(int argc, char *argv[])
 {
-  // your code here.  you should write the secret to fd 2 using write
-  // (e.g., write(2, secret, 8)
 
   printf("attack start\n");
   char *end = sbrk(PGSIZE * 32);
