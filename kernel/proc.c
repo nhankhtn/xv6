2e00942fe0e33a99f13f9e6a8b99893517198e5f
nhankhtn <nguyenanhnguyen3006@gmail.com>
2024-12-26 22:09:55 +0700

    final feature speed up system call

--- kernel/proc.c
+++ kernel/proc.c
@@ -140,6 +140,19 @@ found:
     return 0;
   }
 
+  // Allocate a usyscall page
+  // NOTE: Must allocate usyscall page before create page table of this
+  //  process (when call proc_pagetable)
+  if ((p->usyscallpage = (struct usyscall *)kalloc()) == 0) {
+    freeproc(p);
+    release(&p->lock);
+    return 0;
+  }
+  // Set up value for pid of usyscall is the pid of this process
+  p->usyscallpage->pid = p->pid;
+  // An empty user page table.
+  p->pagetable = proc_pagetable(p);
+
   // Set up new context to start executing at forkret,
   // which returns to user space.
   memset(&p->context, 0, sizeof(p->context));
@@ -158,6 +171,9 @@ freeproc(struct proc *p)
   if(p->trapframe)
     kfree((void*)p->trapframe);
   p->trapframe = 0;
+  // if(p->usyscallpage)
+  //   kfree((void *)p->usyscallpage);
+  p->usyscallpage = 0;
   if(p->pagetable)
     proc_freepagetable(p->pagetable, p->sz);
   p->pagetable = 0;
@@ -202,6 +218,15 @@ proc_pagetable(struct proc *p)
     return 0;
   }
 
+    //map the usyscall page at USYSCALL, share for kernel and user
+  if(mappages(pagetable, USYSCALL, PGSIZE,
+  (uint64)(p->usyscallpage), PTE_R | PTE_U) < 0) {
+    uvmunmap(pagetable, TRAMPOLINE, 1, 0);
+    uvmunmap(pagetable, TRAPFRAME, 1, 0);
+    uvmfree(pagetable, 0);
+    return 0;
+  }
+
   return pagetable;
 }
 
@@ -212,6 +237,7 @@ proc_freepagetable(pagetable_t pagetable, uint64 sz)
 {
   uvmunmap(pagetable, TRAMPOLINE, 1, 0);
   uvmunmap(pagetable, TRAPFRAME, 1, 0);
+   uvmunmap(pagetable,USYSCALL, 1, 0);
   uvmfree(pagetable, sz);
 }
 
